92fd36b611979e70782959e359be48b09618d275
maamokun <maamo-kun@outlook.com>
2024-02-28 18:26:09 +0900

    Refactor server.js and index.js files

--- index.js
+++ index.js
@@ -6,28 +6,29 @@ import { runServer } from "./server";
 config();
 
 console.log("Validating .env file...");
-console.log("Tip: You can customize the iframe by setting the following environment variables: BORDER_COLOR, BACKGROUND_COLOR, TEXT_COLOR");
+console.log(
+	"Tip: You can customize the iframe by setting the following environment variables: BORDER_COLOR, BACKGROUND_COLOR, TEXT_COLOR",
+);
 
-if(!process.env.HETRIX_KEY) {
-    console.error("No HetrixTools API key found in .env file");
-    process.exit(1);
+if (!process.env.HETRIX_KEY) {
+	console.error("No HetrixTools API key found in .env file");
+	process.exit(1);
 }
-if(!process.env.ADMIN_KEY) {
-    console.error("No Administrator key found in .env file");
-    process.exit(1);
+if (!process.env.ADMIN_KEY) {
+	console.error("No Administrator key found in .env file");
+	process.exit(1);
 }
-if(!process.env.HETRIX_AUTH) {
-    console.error("No Webhook Authentication Token found in .env file");
-    process.exit(1);
+if (!process.env.HETRIX_AUTH) {
+	console.error("No Webhook Authentication Token found in .env file");
+	process.exit(1);
 }
-if(!process.env.UPDATE_INTERVAL) {
-    console.warn("No Update Interval specified, defaulting to 6 hours");
+if (!process.env.UPDATE_INTERVAL) {
+	console.warn("No Update Interval specified, defaulting to 6 hours");
 }
-if(!process.env.PORT) {
-    console.warn("No Port specified, defaulting to 3000");
+if (!process.env.PORT) {
+	console.warn("No Port specified, defaulting to 3000");
 }
 
 ForceUpdateData();
 runClient();
 runServer();
-
--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { getData, RemoveOnline, AddOnline, ForceUpdateData } from "./HetrixClient";
+import {
+	getData,
+	RemoveOnline,
+	AddOnline,
+	ForceUpdateData,
+} from "./HetrixClient";
 import { config } from "dotenv";
 import path from "path";
 config();
@@ -8,69 +13,85 @@ const key = process.env.HETRIX_AUTH;
 const expectedAuth = `Bearer ${key}`;
 
 export function runServer() {
+	const app = express();
+	app.use(express.json());
+	app.set("view engine", "ejs");
+	app.set("views", path.join(__dirname, "views"));
 
-    const app = express();
-    app.use(express.json());
-    app.set('view engine', 'ejs');
-    app.set('views', path.join(__dirname, 'views'));
+	const port = process.env.PORT || 3000;
+	const key = process.env.ADMIN_KEY;
+	const border = process.env.BORDER_COLOR || "#fff";
+	const bgColor = process.env.BACKGROUND_COLOR || "#000";
+	const textColor = process.env.TEXT_COLOR || "#fff";
 
-    const port = process.env.PORT || 3000;
-    const key = process.env.ADMIN_KEY;
-    const border = process.env.BORDER_COLOR || "#fff";
-    const bgColor = process.env.BACKGROUND_COLOR || "#000";
-    const textColor = process.env.TEXT_COLOR || "#fff";
+	app.get("/", async (req, res) => {
+		const json = await getData();
+		const total = json.TotalMonitors;
+		const down_monit = json.DownMonitors;
 
-    app.get("/", async (req, res) => {
-        const json = await getData();
-        const total = json.TotalMonitors;
-        const down_monit = json.DownMonitors;
+		if (down_monit > 0) {
+			res.render("iframe", {
+				dotColor: "#ffff00",
+				customText: "Partial outage",
+				borderColor: border,
+				bgColor: bgColor,
+				textColor: textColor,
+			});
+		}
+		if (down_monit == 0) {
+			res.render("iframe", {
+				dotColor: "#00ff00",
+				customText: "All systems online",
+				borderColor: border,
+				bgColor: bgColor,
+				textColor: textColor,
+			});
+		}
+		if (down_monit == total) {
+			res.render("iframe", {
+				dotColor: "#ff0000",
+				customText: "Major outage",
+				borderColor: border,
+				bgColor: bgColor,
+				textColor: textColor,
+			});
+		}
+	});
 
-        if (down_monit > 0) {
-            res.render('iframe', { dotColor: '#ffff00', customText: 'Partial outage', borderColor: border, bgColor: bgColor, textColor: textColor });
-        }
-        if (down_monit == 0) {
-            res.render('iframe', { dotColor: '#00ff00', customText: 'All systems online', borderColor: border, bgColor: bgColor, textColor: textColor});
-        }
-        if (down_monit == total) {
-            res.render('iframe', { dotColor: '#ff0000', customText: 'Major outage', borderColor: border, bgColor: bgColor, textColor: textColor });
-        }
-    });
+	app.get("/raw", async (req, res) => {
+		const json = await getData();
+		res.json(json);
+	});
 
+	app.get("/update", async (req, res) => {
+		const keyEntry = req.query.key;
+		if (keyEntry !== key) {
+			res.status(401).send("Unauthorized");
+			return;
+		}
+		console.log("Remote update request received!");
+		ForceUpdateData();
+		res.send("Data Resynced!");
+	});
 
-    app.get("/raw", async (req, res) => {
-        const json = await getData();
-        res.json(json);
-    });
+	app.post("/ingest", (req, res) => {
+		const json = req.body;
+		const auth = req.headers.authorization;
+		console.log("Data received!");
+		if (auth !== expectedAuth) {
+			console.error("Unauthorized request");
+			res.status(401).send("Unauthorized");
+			return;
+		}
+		if (json.monitor_status === "offline") {
+			RemoveOnline();
+		}
+		if (json.monitor_status === "online") {
+			AddOnline();
+		}
+	});
 
-    app.get("/update", async (req, res) => {
-        const keyEntry = req.query.key;
-        if(keyEntry !== key) {
-            res.status(401).send("Unauthorized");
-            return;
-        }
-        console.log("Remote update request received!");
-        ForceUpdateData();
-        res.send("Data Resynced!");
-    });
-
-    app.post("/ingest", (req, res) => {
-        const json = req.body;
-        const auth = req.headers.authorization;
-        console.log("Data received!");
-        if(auth !== expectedAuth) {
-            console.error("Unauthorized request");
-            res.status(401).send("Unauthorized");
-            return;
-        }
-        if (json.monitor_status === "offline") {
-            RemoveOnline();
-        }
-        if (json.monitor_status === "online") {
-            AddOnline();
-        }
-    });
-
-    app.listen(port, () => {
-        console.log(`Server is running on port ${port}`);
-    });
+	app.listen(port, () => {
+		console.log(`Server is running on port ${port}`);
+	});
 }
